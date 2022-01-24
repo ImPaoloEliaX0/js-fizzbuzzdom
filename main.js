c3eb0ad159613b6aba6cb9b03b2caad1fe3430d6
Paolo Elia <92871203+ImPaoloEliaX0@users.noreply.github.com>
2022-01-24 09:59:34 +0100

    Update main.js
    Fine esercizio

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
+
+
 let containerElement = document.getElementById('container');
 
-for(let i=0; i<=1000;i++) {
+let fizChoice ="" ;
 
-    if(i%3 == 0 && i%5==0){  /*multiplo di 3 e 5 */
+for(let i=2; i<=1000;i++) {
 
-        containerElement.innerHTML += "<div class='boxblue'>" + i +" </div>";
+    if(i%3 == 0 && i%5==0){  /*multiplo di 3 e 5 insieme */
 
-    }else if(i%5 == 0 && i%3 != 0) {
+        fizChoice = "fizzbuz";
 
-        containerElement.innerHTML += "<div class='boxyellow'>" + i +" </div>";
+        containerElement.innerHTML += "<div class='box red'>" + fizChoice +" </div>";
 
-    }else if (i%3 == 0 && i%5 !=0)
-    containerElement.innerHTML += "<div class='boxred'>" + i +" </div>";
+    }else if(i%5 == 0 && i%3 != 0) { /*multiplo di 5 */
+
+        fizChoice = "buzz";
+
+        containerElement.innerHTML += "<div class='box yellow'>" + fizChoice +" </div>";
+
+    }else if (i%3 == 0 && i%5 !=0) {   /*multiplo di 3 */
+
+        fizChoice = "fizz";
+
+    containerElement.innerHTML += "<div class='box green'>" + fizChoice +" </div>";
+
+    }else {   /*numeri normali */
+
+        containerElement.innerHTML += "<div class='box '>" + i +" </div>";
+    }
 }
